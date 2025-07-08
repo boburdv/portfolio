571965feb36d9e4dfd4e148e0e0dcf235dc6e0db
BoburDv <exnessbobur@gmail.com>
2025-07-08 11:42:21 -0700

    Portfolio

--- src/app/solution/page.jsx
+++ src/app/solution/page.jsx
@@ -6,10 +6,14 @@ export default function Solution() {
   return (
     <div className="mt-[10%] flex items-center justify-center px-4">
       <div className="flex flex-col sm:flex-row items-center gap-8 text-center sm:text-left">
-        <img src="/bossbaby.png" className="w-[120px] sm:w-[150px] animate-slide-top delay-600" alt="bossbaby" />
+        <img
+          src="/bossbaby.png"
+          className="w-[120px] sm:w-[150px] animate-slide-top delay-600 filter grayscale"
+          alt="bossbaby"
+        />
 
         <div>
-          <h2 className="text-5xl sm:text-7xl font-bold text-slate-500 mb-2 animate-slide-top delay-100">404</h2>
+          <h2 className="text-5xl sm:text-7xl font-bold text-slate-600 mb-2 animate-slide-top delay-100">404</h2>
           <div className="mb-4 animate-slide-top delay-200">
             <p className="text-muted-foreground font-semibold text-lg">
               Bunday sahifa mavjud emas! <br />
