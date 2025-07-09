d424ffb3658423af84ea2af21629cd5aeeab0e88
BoburDv <exnessbobur@gmail.com>
2025-07-09 01:39:19 -0700

    Portfolio

--- src/app/solution/page.jsx
+++ src/app/solution/page.jsx
@@ -8,19 +8,19 @@ export default function Solution() {
       <div className="flex flex-col sm:flex-row items-center gap-8 text-center sm:text-left">
         <img
           src="/bossbaby.png"
-          className="w-[120px] sm:w-[150px] animate-slide-top delay-600 filter grayscale"
+          className="w-[120px] sm:w-[150px] animate-slide-top delay-100 filter grayscale"
           alt="bossbaby"
         />
 
         <div>
-          <h2 className="text-5xl sm:text-7xl font-bold text-slate-600 mb-2 animate-slide-top delay-100">404</h2>
-          <div className="mb-4 animate-slide-top delay-200">
+          <h2 className="text-5xl sm:text-7xl font-bold text-slate-600 mb-2 animate-slide-top delay-200">404</h2>
+          <div className="mb-4 animate-slide-top delay-300">
             <p className="text-muted-foreground font-semibold text-lg">
               Bunday sahifa mavjud emas! <br />
             </p>
             <span className="text-slate-500 text-lg mb-4">Bu boshlanishidan oldingi jimjitlik...</span>
           </div>
-          <div className="animate-slide-top delay-500">
+          <div className="animate-slide-top delay-600">
             <Link href="/" className={buttonVariants({ variant: "default" })}>
               Bosh sahifaga qaytish
             </Link>
